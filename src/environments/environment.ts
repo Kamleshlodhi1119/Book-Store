c72cf6be0a6b9b112af2741aeb96bceb7d83f94a
Kamleshlodhi1119 <kamleshlodhi9302#gmail.com>
2026-01-25 18:39:33 +0530

    button updsted

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,16 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://bookstore-h5qp.onrender.com/api',
-  apiBaseUrl:'https://bookstore-h5qp.onrender.com/api',
-  bookImageBaseUrl: 'https://bookstore-h5qp.onrender.com'
-};
-
-
 // export const environment = {
 //   production: false,
-//   apiUrl: 'http://localhost:8082/api',
-//   apiBaseUrl:'http://localhost:8082/api',
-//   // bookImageBaseUrl: 'http://localhost:8082/api/books'
-//   bookImageBaseUrl: 'http://localhost:8082'
-
+//   apiUrl: 'https://bookstore-h5qp.onrender.com/api',
+//   apiBaseUrl:'https://bookstore-h5qp.onrender.com/api',
+//   bookImageBaseUrl: 'https://bookstore-h5qp.onrender.com'
 // };
+
+
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8082/api',
+  apiBaseUrl:'http://localhost:8082/api',
+  // bookImageBaseUrl: 'http://localhost:8082/api/books'
+  bookImageBaseUrl: 'http://localhost:8082'
+
+};
