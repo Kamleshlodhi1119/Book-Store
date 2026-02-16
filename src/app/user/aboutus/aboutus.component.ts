dfc642bdc9fac73b9d18a712da6bea05dc3c9376
Kamleshlodhi1119 <kamleshlodhi9302#gmail.com>
2026-02-16 23:49:02 +0530

    About us updated

--- src/app/user/aboutus/aboutus.component.ts
+++ src/app/user/aboutus/aboutus.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AlertService } from 'src/app/core/services/alert.service';
 import { BookService } from 'src/app/core/services/book.service';
@@ -11,7 +11,69 @@ import { environment } from 'src/environments/environment';
   templateUrl: './aboutus.component.html',
   styleUrls: ['./aboutus.component.css']
 })
-export class AboutusComponent {
-
+export class AboutusComponent  implements OnInit {
   readonly imageApi = environment.bookImageBaseUrl;
+
+  books: any[] = [];
+ authors: any[] = [];
+  displayAuthors: any[] = [];
+  MAX_AUTHORS = 4;
+
+
+  constructor(
+    private bookService: BookService,
+    private cartService: CartService,
+    private wishlistService: WishlistService,
+    private router: Router,
+    private alertService: AlertService
+  ) {}
+
+  ngOnInit(): void {
+
+    this.alertService.show('this application might be take time due to free tire of backend', 'warning');
+    this.bookService.getAll().subscribe({
+      next: res => this.books = res as any[],
+      error: () => this.alertService.show('Failed to load books', 'error')
+    });
+
+      this.loadAuthors();
+  }
+loadAuthors() {
+    this.bookService.getAuthors().subscribe({
+      next: res => {
+        this.authors = res;
+        this.pickRandomAuthors();
+      },
+      error: err => {
+        console.error('Failed to load authors', err);
+      }
+    });
+  }
+
+  pickRandomAuthors() {
+    const shuffled = [...this.authors]
+      .sort(() => 0.5 - Math.random());
+
+    this.displayAuthors = shuffled.slice(0, this.MAX_AUTHORS);
+  }
+
+  addToCart(bookId: number) {
+    this.cartService.add({ bookId, quantity: 1 }).subscribe({
+      next: msg => this.alertService.show('Added to cart', 'success'),
+      error: () => this.alertService.show('Add to cart failed', 'error')
+    });
+  }
+
+  addToWishlist(bookId: number) {
+    this.wishlistService.add(bookId).subscribe({
+      next: () => this.alertService.show('Added to wishlist', 'success'),
+      error: () => this.alertService.show('Wishlist failed', 'error')
+    });
+  }
+
+ viewBook(id: number) {
+  this.router.navigate(['/book-details', id]);
 }
+
+}
+
